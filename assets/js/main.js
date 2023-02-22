f18107643f22eb57801f1c18e7e9ec08516f5019
JeevArn <123997072+JeevArn@users.noreply.github.com>
2023-02-23 00:04:13 +0100

    Ajout de javascript
    - ajout de devoir-string.html et devoirstring.js pour le devoir 4
    - ajout du main.js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+function date_heure() {
+	var now = new Date();
+	var annee = now.getFullYear();
+	var mois = ('0'+now.getMonth()+1).slice(-2);
+	var jour = ('0'+now.getDate()).slice(-2);
+	var heure = ('0'+now.getHours()).slice(-2);
+	var minute = ('0'+now.getMinutes()).slice(-2);
+	var seconde = ('0'+now.getSeconds()).slice(-2);
+ 	document.getElementById("holder1").innerHTML = "Nous sommes le "+jour+"/"+mois+"/"+annee+" et il est "+heure+"h "+minute+"min "+seconde+"s.";
+ }
+
+
+
+
+
+
+ 
