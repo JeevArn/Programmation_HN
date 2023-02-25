e7ad1a1074c123c1d0e068a858e993b860c404c6
JeevArn <123997072+JeevArn@users.noreply.github.com>
2023-02-25 18:55:33 +0100

    modif main.js
    ajout btn2 pour afficher en maj date_heure()

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,13 @@ function date_heure() {
 	var minute = ('0'+now.getMinutes()).slice(-2);
 	var seconde = ('0'+now.getSeconds()).slice(-2);
  	document.getElementById("holder1").innerHTML = "Nous sommes le "+jour+"/"+mois+"/"+annee+" et il est "+heure+"h "+minute+"min "+seconde+"s.";
+ 	document.getElementById("btn2").style.display= "inline";
  }
 
-
-
-
+function maj() {
+	let text = document.getElementById("holder1").innerHTML;
+	document.getElementById("holder1").innerHTML = text.toUpperCase();
+}
 
 
  
